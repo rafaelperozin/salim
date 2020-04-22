3a07acddf5f98f11217c4070ed5a41e80260e853
Rafael Perozin <rafaelperozin@outlook.com>
2020-04-22 03:17:51 +0100

    response totals on the header

--- server/src/controllers/TransactionController.js
+++ server/src/controllers/TransactionController.js
@@ -37,11 +37,68 @@ module.exports = {
 
         let firstDay = year + '-' + (Number(month) < 10 ? + '0' + month.toString() : month) + '-01';
         let lastDay = year + '-' + (Number(month) < 10 ? + '0' + month.toString() : month) + '-31';
+        let firstDayLast = year + '-' + (Number(month) < 10 ? + '0' + (month-1).toString() : (month-1)) + '-01';
+        let lastDayLast = year + '-' + (Number(month) < 10 ? + '0' + (month-1).toString() : (month-1)) + '-31';
 
         const transactions = await connection('transaction')
             .where('user_id', user_id)
             .whereBetween('date', [firstDay, lastDay])
             .select('*');
+        
+        const sum_cur_incomes_to_pay = await connection('transaction')
+            .sum('value')
+            .where('user_id', user_id)
+            .where('type', 'income')
+            .where('status', 'to pay')
+            .whereBetween('date', [firstDay, lastDay]);
+        const sum_cur_incomes_paid = await connection('transaction')
+            .sum('value')
+            .where('user_id', user_id)
+            .where('type', 'income')
+            .where('status', 'paid')
+            .whereBetween('date', [firstDay, lastDay]);
+        const sum_cur_expenses_to_pay = await connection('transaction')
+            .sum('value')
+            .where('user_id', user_id)
+            .where('type', 'expense')
+            .where('status', 'to pay')
+            .whereBetween('date', [firstDay, lastDay]);
+        const sum_cur_expenses_paid = await connection('transaction')
+            .sum('value')
+            .where('user_id', user_id)
+            .where('type', 'expense')
+            .where('status', 'paid')
+            .whereBetween('date', [firstDay, lastDay]);
+        const sum_last_incomes = await connection('transaction')
+            .sum('value')
+            .where('user_id', user_id)
+            .where('type', 'income')
+            .whereBetween('date', [firstDayLast, lastDayLast]);
+        const sum_last_expenses = await connection('transaction')
+            .sum('value')
+            .where('user_id', user_id)
+            .where('type', 'expense')
+            .whereBetween('date', [firstDayLast, lastDayLast]);
+        
+        const cur_incomes_to_pay = sum_cur_incomes_to_pay[0]['sum(`value`)'] == null 
+            ? 0 : sum_cur_incomes_to_pay[0]['sum(`value`)'];
+        const cur_incomes_paid = sum_cur_incomes_paid[0]['sum(`value`)'] == null 
+            ? 0 : sum_cur_incomes_paid[0]['sum(`value`)'];
+        const cur_expenses_to_pay = sum_cur_expenses_to_pay[0]['sum(`value`)'] == null 
+            ? 0 : sum_cur_expenses_to_pay[0]['sum(`value`)'];
+        const cur_expenses_paid = sum_cur_expenses_paid[0]['sum(`value`)'] == null 
+            ? 0 : sum_cur_expenses_paid[0]['sum(`value`)'];
+        const last_incomes = sum_last_incomes[0]['sum(`value`)'] == null 
+            ? 0 : sum_last_incomes[0]['sum(`value`)'];
+        const last_expenses = sum_last_expenses[0]['sum(`value`)'] == null 
+            ? 0 : sum_last_expenses[0]['sum(`value`)'];
+        
+        res.header('X-Current-Total-Incomes-To-Pay', cur_incomes_to_pay);
+        res.header('X-Current-Total-Incomes-Paid', cur_incomes_paid);
+        res.header('X-Current-Total-Expenses-To-Pay', cur_expenses_to_pay);
+        res.header('X-Current-Total-Expenses-Paid', cur_expenses_paid);
+        res.header('X-Last-Total-Incomes-', last_incomes);
+        res.header('X-Last-Total-Expenses', last_expenses);
         
         return res.json(transactions);
     },
