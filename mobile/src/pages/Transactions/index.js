2925b66cf9e858314a6f9adbd22a9da43045ff1b
Rafael Perozin <rafael.perozin@xigen.co.uk>
2020-08-19 20:55:56 +0100

    transactions list jsx structure

--- mobile/src/pages/Transactions/index.js
+++ mobile/src/pages/Transactions/index.js
@@ -31,6 +31,38 @@ export default function Budgets() {
                         <Button style={ThemeButtons.simple}><Text style={ThemeButtons.simpleText}>+ New Transaction</Text></Button>
                         
                     </View>
+
+                    <View style={styles.tabs}>
+                        <Buttom style={styles.tabButton}><Text style={styles.tabText}>All</Text></Buttom>
+                        <Buttom style={styles.tabButton}><Text style={styles.tabText}>Incomes</Text></Buttom>
+                        <Buttom style={styles.tabButton}><Text style={styles.tabText}>Expenses</Text></Buttom>
+                        <Buttom style={styles.tabButton}><Text style={styles.tabText}>Filters</Text></Buttom>
+                    </View>
+
+                    <View style={styles.transactions}>
+
+                        <View style={styles.transactionsAmount}>
+                            <Text style={styles.transactionsTitle}>Previews Period Balance</Text>
+                            <Text style={[styles.transactionsTitle, theme.positiveValue]}>£504.90</Text>
+                        </View>
+
+                        <View style={styles.transactionsList}>
+                            <View style={styles.transactionContainer} transaction-type="Expenses">
+                                <Text style={styles.transactionText}>12</Text>
+                                <View style={[styles.transactionCategory, styles.negativeTransCat]}>
+                                    <Text style={[styles.transactionCategoryName, styles.negativeTransCatName]}>Groceries</Text>
+                                </View>
+                                <Text style={[styles.transactioText, theme.negativeText]}>Previews Period Balance</Text>
+                                <Text style={[styles.transactionText, theme.negativeValue]}>£504.90</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.transactionsAmount}>
+                            <Text style={styles.transactionsTitle}>Previews Period Balance</Text>
+                            <Text style={[styles.transactionsTitle, theme.negativeValue]}>£504.90</Text>
+                        </View>
+
+                    </View>
                     
                 </Content>
                 
